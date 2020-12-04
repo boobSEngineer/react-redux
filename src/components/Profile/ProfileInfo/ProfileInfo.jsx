f310e72f099f5759ffd51d6b84e70886fa485a44
boobSEngineer <helenz.eltech@gmail.com>
2020-12-04 17:34:35 +0300

    base photo

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Preloader from "../../common/Preloader/Preloader";
+import basePhoto from '../../../assets/base.png';
 
 
 const ProfileInfo = (props) => {
@@ -12,7 +13,7 @@ const ProfileInfo = (props) => {
                 <img src='https://www.ixbt.com/img/n1/news/2020/6/1/SI_3DSVC_SuperMarioBros_image1280w_large.jpg'/>
             </div>
             <div>
-                <img src={props.profile.photos.large}/>
+                <img src={!props.profile.photos.large ?basePhoto :props.profile.photos.large}/>
             </div>
             <div>
                 {props.profile.fullName}
